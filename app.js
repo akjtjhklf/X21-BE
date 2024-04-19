1f38c8b9f8dd62811193d003770b80ffdae02f18
Nguyen Khanh <nguyenkhanh28031999@gmail.com>
2024-04-19 21:41:39 +0700

    nothing

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const qnaRouter = require("./src/routes/qnaRouter");
+const subjectRouter = require("./src/routes/subjectRouter");
+
 require("dotenv").config({ path: "./.env" });
 
 const app = express();
 
 const port = process.env.PORT;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/", qnaRouter);
+app.use("/", subjectRouter);
+
 app.get("/", (req, res) => {
   res.send("hello");
 });

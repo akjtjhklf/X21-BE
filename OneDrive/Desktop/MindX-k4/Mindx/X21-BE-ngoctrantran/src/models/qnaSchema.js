cdde0ddffdaca539cfbf009b20fdcd51098173c6
akjtjhklf <trngtran358@gmail.com>
2024-05-05 14:07:54 +0700

    ngoctran update

---
+++ OneDrive/Desktop/MindX-k4/Mindx/X21-BE-ngoctrantran/src/models/qnaSchema.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+let qnaSchema = new mongoose.Schema({
+  question: {
+    type: String,
+    required: true,
+  },
+  subjectId: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+    ref: "subjects",
+  },
+  answers: [
+    {
+      type: String,
+      required: false,
+    },
+  ],
+  challengeType: {
+    type: String,
+    enum: ["qnas", "hangman", "arrange"],
+    required: true,
+  },
+  rightAnswer: [{
+    type: mongoose.Schema.Types.Mixed,
+    required: true,
+  }
+  ],
+});
+
+module.exports = mongoose.model("qnas", qnaSchema);
